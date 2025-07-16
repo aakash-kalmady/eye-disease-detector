f639e4625a777b91794010df4e2679182f57251a
Aakash Kalmady <arkalmady@gmail.com>
2025-07-16 13:20:53 -0400

    create initial flask app

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
-print("hello world")
+import os
+import cv2
+import numpy as np
+import tensorflow as tf
+from flask import Flask, request, render_template, jsonify
+
+# Initialize the Flask application
+app = Flask("eye-disease-detector")
+
+# --- Flask REST API Routes ---
+@app.route('/', methods=['GET'])
+def index():
+    """Renders the main upload page."""
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    return("Hello world")
+
+# --- Run the App ---
+if __name__ == '__main__':
+    # Use port 8080 to avoid conflicts with other services
+    app.run(host='0.0.0.0', port=8080, debug=True)
